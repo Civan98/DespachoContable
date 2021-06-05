424974371255cdf88fdf8cf4a977ce687596a589
Ivan Catañeda Contreras <52634607+Civan98@users.noreply.github.com>
2021-06-04 23:17:09 -0500

    correcion de diseño

--- php/cita.php
+++ php/cita.php
@@ -12,74 +12,75 @@
 <body>
 
     <header>
-        <a href="#" class="logo">Despacho Contable</a>
+        <a href="#" class="logo">Contreras y asociados</a>
         <div class="toggle"></div>
         <ul class="navigation">
-            <li><a href="../index.html" style="--i:1;" class="active">Inicio</a></li>
+            <li><a href="../index.html" style="--i:1;">Inicio</a></li>
             <li><a href="#" style="--i:2;">¿Quiénes somos?</a></li>
             <li><a href="#" style="--i:3;">Servicios</a></li>
             <li><a href="#" style="--i:4;">Contacto</a></li>
-            <li><a href="php/cita.php" style="--i:5;">Agendar Cita</a></li>
+            <li><a href="php/cita.php" style="--i:5;" class="active">Agendar Cita</a></li>
         </ul>
     </header>
 
-    <h1>Agendar Cita</h1>
-    <form id="formulario" action="php/agendarCita.php" method="post">
-        <label for="nombres">Nombre:</label>
-        <input type="text" name="nombre" required><br>
+    <section>
 
-        <label for="apellidos">Apellidos:</label>
-        <input type="text" name="apellidos" required><br>
+        <div class="content">
+            <div class="textbox">
+                <h1>Agendar Cita</h1>
+                <form id="formulario" action="agendarCita.php" method="post">
+                    <input type="text" name="nombre" placeholder="Nombre" required><br>
 
-        <label for="correo">Correo:</label>
-        <input type="email" name="correo" required><br>
+                    <input type="text" name="apellidos" placeholder="Apellidos" required><br>
 
-        <label for="celular">Número de celular:</label>
-        <input type="number" name="celular" required maxlength="10" oninput="if(this.value.length > this.maxLength) this.value = this.value.slice(0, this.maxLength);"><br>
+                    <input type="email" name="correo" placeholder="Correo" required><br>
 
-        <select name="hora" id="hora" disabled>
-            <option value="9">9:00 AM</option>
-            <option value="10">10:00 AM</option>
-            <option value="11">11:00 AM</option>
-            <option value="12">12:00 PM</option>
-            <option value="01">01:00 PM</option>
-            <option value="02">02:00 PM</option>
-            <option value="03">03:00 PM</option>
-            <option value="04">04:00 PM</option>
-            <option value="05">05:00 PM</option>
-            <option value="06">06:00 PM</option>
-        </select><br>
+                    <input type="number" name="celular" placeholder="Número de celular" required maxlength="10" oninput="if(this.value.length > this.maxLength) this.value = this.value.slice(0, this.maxLength);"><br>
 
-        <button id="aceptar" type="submit" disabled='true'>Agendar</button>
-    </form>
+                    <select name="hora" id="hora" disabled>
+                        <option value="9">9:00 AM</option>
+                        <option value="10">10:00 AM</option>
+                        <option value="11">11:00 AM</option>
+                        <option value="12">12:00 PM</option>
+                        <option value="01">01:00 PM</option>
+                        <option value="02">02:00 PM</option>
+                        <option value="03">03:00 PM</option>
+                        <option value="04">04:00 PM</option>
+                        <option value="05">05:00 PM</option>
+                        <option value="06">06:00 PM</option>
+                    </select><br>
 
+                    <button id="aceptar" type="submit" disabled='true'>Agendar</button>
+                </form>
+            </div>
+        </div>
 
-    <div id="calendar">
-        <table border="1" id="tabla">
-            <thead>
-                <tr>
-                    <th>
-                        <button onclick="mesAnt()">
-                            << /button>
-                    </th>
-                    <th id="mes" colspan="5"></th>
-                    <th>
-                        <button onclick="mesPost()">></button>
-                    </th>
-                </tr>
-            </thead>
-            <tbody id="tablaBody">
-                <tr id="dias"></tr>
-            </tbody>
-        </table>
-    </div>
-    <h2 id="fechaCita"></h2>
-
+        <div id="calendar">
+            <table border="1" id="tabla">
+                <thead>
+                    <tr>
+                        <th>
+                            <button onclick="mesAnt()">
+                                < </button>
+                        </th>
+                        <th id="mes" colspan="5"></th>
+                        <th>
+                            <button onclick="mesPost()">></button>
+                        </th>
+                    </tr>
+                </thead>
+                <tbody id="tablaBody">
+                    <tr id="dias"></tr>
+                </tbody>
+            </table>
+        </div>
+        <h2 id="fechaCita"></h2>
+    </section>
     <script src="../js/app.js" type="text/JavaScript"></script>
     <script>
         const menuToggle = document.querySelector('.toggle');
         const menuNavigation = document.querySelector('.navigation');
-        menuToggle.onclick = function () {
+        menuToggle.onclick = function() {
             menuToggle.classList.toggle('active');
             menuNavigation.classList.toggle('active');
         }
